37d11d671f729ba176988d7d919db05e07aaa57d
Ali Burzhi <aliburzhi033@gmail.com>
2021-07-29 20:07:30 +0300

    done

---
+++ task-01.js
@@ -0,0 +1,8 @@
+const navAllLiItemsRef = document.querySelectorAll('ul#categories li.item');
+
+console.log('В списке ', navAllLiItemsRef.length, 'категории ');
+
+navAllLiItemsRef.forEach(element => {
+         console.log('Категория:', element.firstElementChild.textContent);
+         console.log('Количество элементов:', element.lastChild.previousSibling.childElementCount);
+      });
